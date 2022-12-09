9643388fff8412bf9d59684591ccf34ba573aded
mezez <ekemammezez@gmail.com>
2022-12-09 14:21:12 +0100

    refactor downloader

--- src/main/java/org/semanticwebproject/Main.java
+++ src/main/java/org/semanticwebproject/Main.java
@@ -2,6 +2,8 @@ package org.semanticwebproject;
 
 public class Main {
     public static void main(String[] args) {
+        //download and read calendar file or read if necessary
+
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/semanticwebproject/lib/Helpers.java
+++ src/main/java/org/semanticwebproject/lib/Helpers.java
@@ -7,10 +7,13 @@ import java.net.URL;
 
 public class Helpers {
 
+    public static String calendarName = "calendar.ics";
+
     public static void downloadICS(String urlString) throws IOException {
         URL url = new URL(urlString);
         BufferedInputStream bis = new BufferedInputStream(url.openStream());
-        FileOutputStream fis = new FileOutputStream("C:\\Users\\admin\\Dowloads\\calendar.ics");
+
+        FileOutputStream fis = new FileOutputStream(calendarName);
         byte[] buffer = new byte[1024];
         int count=0;
         while((count = bis.read(buffer,0,1024)) != -1)
