52c4c364d81cf42065edfce7b72a0c9afde80246
IgnasBarakauskas <b.ignas05@gmail.com>
2023-01-09 23:14:06 +0100

    added CORS fix

--- src/main/java/org/semanticwebproject/Main.java
+++ src/main/java/org/semanticwebproject/Main.java
@@ -49,6 +49,29 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+//      To remove CORS
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         /**
          * API ENDPOINTS START
 
